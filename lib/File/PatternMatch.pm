2647868d1fe4b07421c694a865454a3f0cafd2aa
Magnus Woldrich <m@japh.se>
2019-01-01 13:44:29 +0100

    add weight and sort by it, making sure to have the most important rule applied

--- lib/File/PatternMatch.pm
+++ lib/File/PatternMatch.pm
@@ -2,7 +2,7 @@
 package File::PatternMatch;
 
 use vars qw($VERSION);
-$VERSION = '0.031';
+$VERSION = '0.034';
 
 require Exporter;
 @ISA = qw(Exporter);
@@ -12,123 +12,143 @@ use strict;
 use Term::ExtendedColor qw(fg bg);
 
 # Yes, this is extremely fugly.
-our %patterns = (
-  'S[0-9]{1}[2-9]{1}E[0-9]{1}[2-9]'  => {
-    256   => fg('magenta23', 'New Episode'),
-    dzen  => "^fg(#ff0000)New Episode^fg()",
-    none  => "New Episode",
+our %patterns                        = (
+  'S[0-9]{1}[2-9]{1}E[0-9]{1}[2-9]' => {
+    256                             => fg('magenta23', 'New Episode'),
+    dzen                            => "^fg(#ff0000)New Episode^fg()",
+    none                            => "New Episode",
+    weight                          => 20,
   },
-  'S01E01'                           => {
-    256   => fg('red1', 'New Show'),
-    dzen  => "^fg(#ffff00)New Show^fg()",
-    none  => "New Show",
+  'S01E01'                          => {
+    256                             => fg('red1', 'New Show'),
+    dzen                            => "^fg(#ffff00)New Show^fg()",
+    none                            => "New Show",
+    weight                          => 100,
   },
-  'S0\dE01'                          => {
-    256   => fg('bold', 'Season Premiere'),
-    dzen  => "^fg(#cccd05)Season Premiere^fg()",
-    none  => "Season Premiere",
+  'S0\dE01'                         => {
+    256                             => fg('bold', 'Season Premiere'),
+    dzen                            => "^fg(#cccd05)Season Premiere^fg()",
+    none                            => "Season Premiere",
+    weight                          => 75,
   },
   'do(c|k?)u(ment.+)?|
   (discovery|history)\.(channel)?|
   national\.geographic|
   colossal\.'                        => {
-    256   =>  fg('green16', 'Documentary'),
-    dzen  =>  "^fg(#87d700)Documentary^fg()",
-    none  =>  "Documentary",
+    256                             => fg('green16', 'Documentary'),
+    dzen                            => "^fg(#87d700)Documentary^fg()",
+    none                            => "Documentary",
+    weight                          => 50,
   },
   'EPL|WWE|UFC|UEFA|Rugby|La\Liga|
   Superleague|Allsvenskan|
   Formula\.Ford'                     => {
-    256   =>  fg(145, 'Sport'),
-    dzen  =>  "^fg(#afaf87)Sport^fg()",
-    none  =>  "Sport",
+    256                             => fg(145, 'Sport'),
+    dzen                            => "^fg(#afaf87)Sport^fg()",
+    none                            => "Sport",
+    weight                          => 5,
   },
-  '(?i)swedish|-se-'                 => {
-    256   =>  fg('cyan8', 'Swedish'),
-    dzen  =>  "^fg(#87ffd7)Swedish^fg(#121212)",
-    none  =>  "Swedish",
+  '(?i)swedish|-se-'                => {
+    256                             => fg('cyan8', 'Swedish'),
+    dzen                            => "^fg(#87ffd7)Swedish^fg(#121212)",
+    none                            => "Swedish",
+    weight                          => 30,
   },
-  '(?i)jay\.leno'                    => {
-    256   =>  fg('purple15', 'Talk Show'),
-    dzen  =>  "^fg(87afff)Talk Show^fg()",
-    none  =>  "Talk Show",
+  '(?i)jay\.leno'                   => {
+    256                             => fg('purple15', 'Talk Show'),
+    dzen                            => "^fg(87afff)Talk Show^fg()",
+    none                            => "Talk Show",
   },
-  'PsyCZ|MYCEL|UPE|HiEM|PSi|gEm'     => {
-    256   =>  "\e[38;5;192mPsychedelic\e[0m",
-    dzen  =>  "^fg(#d7ff87)Psychedelic^fg()",
-    none  =>  "Psychedelic",
+  'PsyCZ|MYCEL|UPE|HiEM|PSi|gEm'    => {
+    256                             => "\e[38;5;192mPsychedelic\e[0m",
+    dzen                            => "^fg(#d7ff87)Psychedelic^fg()",
+    none                            => "Psychedelic",
+    weight                          => 70,
   },
   '.+-(H3X|wAx|CMS|BFHMP3|WHOA|RNS|
   C4|CR|UMT|0MNi)(.+)?|FRAY(.+)?$'   => {
-    256   =>  "\e[38;5;094mHip-Hop\e[0m",
-    dzen  =>  "^fg(#309184)Hip-Hop^fg()",
-    none  =>  "Hip-Hop",
+    256                             => "\e[38;5;094mHip-Hop\e[0m",
+    dzen                            => "^fg(#309184)Hip-Hop^fg()",
+    none                            => "Hip-Hop",
+    weight                          => 85,
   },
-  'LzY|qF|SRP|NiF'                   => {
-    256   =>  "\e[38;5;126mRock\e[0m",
-    dzen  =>  "^fg(#af0087)Rock^fg()",
-    none  =>  "Rock",
+  'LzY|qF|SRP|NiF'                  => {
+    256                             => "\e[38;5;126mRock\e[0m",
+    dzen                            => "^fg(#af0087)Rock^fg()",
+    none                            => "Rock",
+    weight                          => 50,
   },
-  '-sour$'                           => {
-    256   =>  "\e[38;5;166mDnB\e[0m",
-    dzen  =>  "^fg(#d75f00)DnB^fg()",
-    none  =>  "DnB",
+  '-sour$'                          => {
+    256                             => "\e[38;5;166mDnB\e[0m",
+    dzen                            => "^fg(#d75f00)DnB^fg()",
+    none                            => "DnB",
+    weight                          => 50,
   },
-  'VA(-|_-_).+'                      => {
-    256   =>  "\e[38;5;049mV/A\e[0m",
-    dzen  =>  "^fg(#00ffaf)V/A^fg()",
-    none  =>  "V/A",
+  'VA(-|_-_).+'                     => {
+    256                             => "\e[38;5;049mV/A\e[0m",
+    dzen                            => "^fg(#00ffaf)V/A^fg()",
+    none                            => "V/A",
+    weight                          => 20,
   },
-  '\(?_?-?CDS-?_?\)?'                => {
-    256   =>  "\e[38;5;244mSingle\e[0m",
-    dzen  =>  "^fg(#808080)Single^fg()",
-    none  =>  "Single",
+  '\(?_?-?CDS-?_?\)?'               => {
+    256                             => "\e[38;5;244mSingle\e[0m",
+    dzen                            => "^fg(#808080)Single^fg()",
+    none                            => "Single",
+    weight                          => 50,
   },
-  '\(?_?-?CDM-?_?\)?'                => {
-    256   => "\e[38;5;233mMaxi\e[0m",
-    dzen  => "^fg(#1c1c1c)Maxi^fg()",
-    none  => "Maxi",
+  '\(?_?-?CDM-?_?\)?'               => {
+    256                             => "\e[38;5;233mMaxi\e[0m",
+    dzen                            => "^fg(#1c1c1c)Maxi^fg()",
+    none                            => "Maxi",
+    weight                          => 50,
   },
-  '\(?_?-?CDA-?_?\)?'                => {
-    256   => "\e[38;5;222mAlbum\e[0m",
-    dzen  => "^fg(#ffd787)Album^fg()",
-    none  => "Album",
+  '\(?_?-?CDA-?_?\)?'               => {
+    256                             => "\e[38;5;222mAlbum\e[0m",
+    dzen                            => "^fg(#ffd787)Album^fg()",
+    none                            => "Album",
+    weight                          => 55,
   },
-  '\(?_?-?DAB-?_?\)?'                => {
-    256   => "\e[38;5;211mDAB\e[0m",
-    dzen  => "^fg(#ff87af)DAB^fg()",
-    none  => "DAB",
+  '\(?_?-?DAB-?_?\)?'               => {
+    256                             => "\e[38;5;211mDAB\e[0m",
+    dzen                            => "^fg(#ff87af)DAB^fg()",
+    none                            => "DAB",
+    weight                          => 50,
   },
-  '\(?_?-?CABLE-?_?\)?'              => {
-    256   => "\e[38;5;191mCable\e[0m",
-    dzen  => "^fg(#d7ff5f)Cable\e[0m",
-    none  => "Cable",
+  '\(?_?-?CABLE-?_?\)?'             => {
+    256                             => "\e[38;5;191mCable\e[0m",
+    dzen                            => "^fg(#d7ff5f)Cable\e[0m",
+    none                            => "Cable",
+    weight                          => 5,
   },
-  '\(?_?-?VLS|Vinyl-?_?\)?'          => {
-    256   => "\e[38;5;201mVinyl\e[0m",
-    dzen  => "^fg(#ff00ff)Vinyl^fg()",
-    none  => "Vinyl",
+  '\(?_?-?VLS|Vinyl-?_?\)?'         => {
+    256                             => "\e[38;5;201mVinyl\e[0m",
+    dzen                            => "^fg(#ff00ff)Vinyl^fg()",
+    none                            => "Vinyl",
+    weight                          => 70,
   },
-  '\(?_?-?WEB-?_?\)?'                => {
-    256   => "\e[38;5;19mWEB\e[0m",
-    dzen  => "^fg(#ffabcd)WEB^fg()",
-    none  => "WEB",
+  '\(?_?-?WEB-?_?\)?'               => {
+    256                             => "\e[38;5;19mWEB\e[0m",
+    dzen                            => "^fg(#ffabcd)WEB^fg()",
+    none                            => "WEB",
+    weight                          => 20,
   },
-  'Live_(on|at|in)'                  => {
-    256   => "\e[38;5;181mLive\e[0m",
-    dzen  => "^fg(#d7afaf)Live^fg()",
-    none  => "Live",
+  'Live_(on|at|in)'                 => {
+    256                             => "\e[38;5;181mLive\e[0m",
+    dzen                            => "^fg(#d7afaf)Live^fg()",
+    none                            => "Live",
+    weight                          => 45,
   },
-  '-Recycled.+$'                     => {
-    256   => "\e[38;5;215Re-release\e[0m",
-    dzen  => "^fg(#ffaf5f)Re-release^fg()",
-    none  => "Re-release",
+  '-Recycled.+$'                    => {
+    256                             => "\e[38;5;215Re-release\e[0m",
+    dzen                            => "^fg(#ffaf5f)Re-release^fg()",
+    none                            => "Re-release",
+    weight                          => 55,
   },
-
-  'TALiON|HB|DV8'       => {
-    256   => "\e[38;5;41m\e[1mHardstyle\e[0m",
-    dzen  => "^fg(#f95504)Hardstyle^fg()",
-    none  => "Hardstyle",
+  'TALiON|HB|DV8'                   => {
+    256                             => "\e[38;5;41m\e[1mHardstyle\e[0m",
+    dzen                            => "^fg(#f95504)Hardstyle^fg()",
+    none                            => "Hardstyle",
+    weight                          => 40,
   },
 );
 
@@ -152,60 +172,60 @@ our %wanted = (
     none  => "",
   },
 
-  'pilot'                      => {
-    dzen  => "^fg(#c02d07)",
-    256   => "\e[38;5;85m\e[1m",
-    none  => "",
+  'pilot'     => {
+    dzen      => "^fg(#c02d07)",
+    256       => "\e[38;5;85m\e[1m",
+    none      => "",
   },
-  'S01E01'                     => {
-    dzen  => "^fg(#d7d75f)",
-    256   => "\e[38;5;185m",
-    none  => "",
+  'S01E01'    => {
+    dzen      => "^fg(#d7d75f)",
+    256       => "\e[38;5;185m",
+    none      => "",
   },
-  'hdtv'                       => {
-    dzen  => "^fg(#cccdda)",
-    256   => "\e[38;5;32m\e[3m",
-    none  => "",
+  'hdtv'      => {
+    dzen      => "^fg(#cccdda)",
+    256       => "\e[38;5;32m\e[3m",
+    none      => "",
   },
-  'pdtv'                       => {
-    dzen  => "^fg(#dacddd)",
-    256   => "\e[38;5;29m\e[3m",
-    none  => "",
+  'pdtv'      => {
+    dzen      => "^fg(#dacddd)",
+    256       => "\e[38;5;29m\e[3m",
+    none      => "",
   },
-  'swedish'                    => {
-    dzen  => "^fg(#ffff00)",
-    256   => "\e[38;5;220m\e[1m",
-    none  => "",
+  'swedish'   => {
+    dzen      => "^fg(#ffff00)",
+    256       => "\e[38;5;220m\e[1m",
+    none      => "",
   },
-  'DIMENSION'                  => {
-    dzen  => "^fg(#faeec4)",
-    256   => "\e[38;5;240m\e[1m",
-    none  => "",
+  'DIMENSION' => {
+    dzen      => "^fg(#faeec4)",
+    256       => "\e[38;5;240m\e[1m",
+    none      => "",
   },
-  'C4'                      => {
-    dzen  => "^fg(#facddc)",
-    256   => "\e[38;5;130m\e[1m",
-    none  => "",
+  'C4'        => {
+    dzen      => "^fg(#facddc)",
+    256       => "\e[38;5;130m\e[1m",
+    none      => "",
   },
-  'LOL'                     => {
-    dzen  => "^fg(#facddc)",
-    256   => "\e[38;5;118m\e[1m",
-    none  => "",
+  'LOL'       => {
+    dzen      => "^fg(#facddc)",
+    256       => "\e[38;5;118m\e[1m",
+    none      => "",
   },
-  '720p'                    => {
-    dzen  => "^fg(#ffcccd)",
-    256   => "\e[38;5;178m\e[1m",
-    none  => "",
+  '720p'      => {
+    dzen      => "^fg(#ffcccd)",
+    256       => "\e[38;5;178m\e[1m",
+    none      => "",
   },
-  'Promo_CD'                => {
-    dzen  => "^fg(#dddc26)",
-    256   => "\e[38;5;173m",
-    none  => "",
+  'Promo_CD'  => {
+    dzen      => "^fg(#dddc26)",
+    256       => "\e[38;5;173m",
+    none      => "",
   },
-  $year                     => {
-    dzen  => "^fg(#93c26b)",
-    256   => "\e[1m",
-    none  => "",
+  $year       => {
+    dzen      => "^fg(#93c26b)",
+    256       => "\e[1m",
+    none      => "",
   },
 );
 
@@ -214,6 +234,14 @@ our %end = (
   256   => "\e[0m",
   none  => "",
 );
+
+my $foo = patternmatch(256, qw(Dirigenten.S01E08.SWEDiSH.720p.WEBRip.X264-AVRATTNING
+                     De.Dagar.Som.Blommorna.Blommar.S01E01.SWEDiSH.720p.WEBRip.X264-AVRATTN
+                     Best.Of.Skavlan.2018.SWEDiSH.720p.HDTV.x264-CCCAM)
+             );
+
+use Data::Dumper;
+
 
 sub patternmatch {
   my $fmt = shift // 'plain';
@@ -230,9 +258,10 @@ sub patternmatch {
       $file =~ s/($keyword)/$wanted{$keyword}->{$fmt}$1$end{$fmt}/gi;
     }
 
-    for my $pattern(keys(%patterns)) {
+    for my $pattern(sort{$patterns{$b}{weight} <=> $patterns{$a}{weight}} keys(%patterns)) {
       if($file =~ /$pattern/x) {
         $results{$i}{$file} = $patterns{$pattern}{$fmt};
+        next;
       }
     }
     $i++;
